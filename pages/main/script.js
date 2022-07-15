4154b09f12c73f00549ed35d24a4746d06dc9c17
Anastasia <87999189+loitez@users.noreply.github.com>
2022-07-16 02:53:58 +0300

    Add files via upload

---
+++ pages/main/script.js
@@ -0,0 +1,261 @@
+const petCards = document.querySelectorAll('.our-friends__card')
+const petList = document.querySelector('.our-friends__list')
+const navItems = document.querySelectorAll('.nav-list__item')
+const navBurger = document.querySelector('.nav-burger')
+const navigation = document.querySelector('.nav')
+const logo = document.querySelector('.logo-wrapper')
+const overlay = document.querySelector('.overlay')
+const sliderRightArrow = document.querySelector('.our-friends__navigation-right')
+const sliderLeftArrow = document.querySelector('.our-friends__navigation-left')
+
+
+// popup
+
+const requestURL = '../pets/pets.json'
+
+const popup = document.querySelector('.popup')
+const name = popup.querySelector('.popup-title')
+const breed = popup.querySelector('.popup-breed')
+const desc = popup.querySelector('.popup-desc')
+const age = popup.querySelector('.age')
+const inoculations = popup.querySelector('.inoculations')
+const diseases = popup.querySelector('.diseases')
+const parasites = popup.querySelector('.parasites')
+const popupImg = popup.querySelector('.popup-img')
+const popupBtn = document.querySelector('.popup-button')
+const body = document.querySelector('body');
+const html = document.querySelector('html');
+const overlayPopup = document.querySelector('.overlay')
+
+
+sendRequest('GET', requestURL)
+    .then ((data) => {
+        petCards.forEach((item) => {
+            item.addEventListener('click', () => {
+                for (let d = 0; d < data.length; d++) {
+                    let title = item.querySelector('.our-friends__card-title').textContent
+                    if (data[d].name === title) {
+                        name.innerHTML = data[d].name
+                        breed.innerHTML = `${data[d].type}: ${data[d].breed}`
+                        desc.innerHTML = data[d].description
+                        age.innerHTML = `<span style="font-weight: 700">Age:</span> ${data[d].age}`
+                        inoculations.innerHTML = `<span style="font-weight: 700">Inoculations:</span> ${data[d].inoculations}`
+                        diseases.innerHTML = `<span style="font-weight: 700">Diseases:</span> ${data[d].diseases}`
+                        parasites.innerHTML = `<span style="font-weight: 700">Parasites:</span> ${data[d].parasites}`
+                        popupImg.src= data[d].img
+                    }
+                }
+                popup.classList.add('active')
+                body.classList.add('disable-popup-scroll')
+                html.classList.add('disable-popup-scroll')
+                overlayPopup.classList.add('active-popup')
+            })
+        })
+
+    })
+
+popupBtn.addEventListener('click', () => {
+    popup.classList.remove('active')
+    body.classList.remove('disable-popup-scroll')
+    html.classList.remove('disable-popup-scroll')
+    overlayPopup.classList.remove('active-popup')
+})
+
+document.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.classList.contains('overlay')) {
+        popup.classList.remove('active')
+        overlayPopup.classList.remove('active-popup')
+        body.classList.remove('disable-popup-scroll')
+        html.classList.remove('disable-popup-scroll')
+    }
+})
+
+document.addEventListener('mouseover', (event) => {
+    const target = event.target;
+    if (target.classList.contains('overlay')) {
+        popupBtn.classList.add('active')
+    }
+})
+document.addEventListener('mouseout', (event) => {
+    const target = event.target;
+    if (target.classList.contains('overlay')) {
+        popupBtn.classList.remove('active')
+    }
+})
+
+
+
+// json pets
+
+
+sendRequest('GET', requestURL)
+    .then ((data) => {
+        console.log('data', data[0])
+        for (let i = 0; i < petCards.length; i++) {
+            petCards[i].querySelector('.our-friends__card-title').innerHTML = data[i].name
+            petCards[i].querySelector('.our-friends__card-img').src = data[i].img
+
+        }
+        console.log(data)
+    })
+    .catch(err => console.log(err))
+
+
+
+function sendRequest(method, url) {
+    return new Promise ((resolve, reject) => {
+        const xhr = new XMLHttpRequest()
+        xhr.open(method, url)
+        xhr.responseType = 'json'
+
+        xhr.onload = () => {
+            if (xhr.status >= 400) {
+                reject(xhr.response)
+            } else {
+                resolve(xhr.response)
+            }
+        }
+        xhr.onerror = () => {
+            reject(xhr.response)
+        }
+        xhr.send()
+    })
+}
+
+
+// slider
+
+sliderRightArrow.addEventListener('click', () => {
+    changeSlides('right')
+})
+sliderLeftArrow.addEventListener('click', () => {
+    changeSlides('left')
+})
+
+
+function changeSlides (direction) {
+
+    // available indexes
+
+    let availableIndexes = []
+    sendRequest('GET', requestURL).then ((data) => {
+        for (let s = 0; s < data.length; s++) {
+            availableIndexes.push(s)
+        }
+        petCards.forEach((item) => {
+            for (let t = 0; t < data.length; t++) {
+                let title = item.querySelector('.our-friends__card-title').textContent
+                if (data[t].name === title) {
+                    availableIndexes.splice(t, 1, '')
+                }
+            }
+        })
+        for (let p = 0; p < availableIndexes.length; p++) {
+            if (availableIndexes[p] === '') {
+                availableIndexes.splice(p, 1)
+                p -= 1;
+            }
+        }
+
+        // random number
+
+        petCards.forEach((item) => {
+            let randomNumber = Math.floor(Math.random()*availableIndexes.length)
+
+            // change slides
+
+            item.querySelector('.our-friends__card-title').innerHTML = data[(availableIndexes[randomNumber])].name
+            item.querySelector('.our-friends__card-img').src = data[(availableIndexes[randomNumber])].img
+
+            if (direction === 'right') {
+                const animationSlides = item.animate([
+                    {transform: 'translate(50px)'},
+                    {transform: 'translate(0)'}
+                ], 500);
+            }
+
+            if (direction === 'left') {
+                const animationSlides = item.animate([
+                    {transform: 'translate(-50px)'},
+                    {transform: 'translate(0)'}
+                ], 500);
+            }
+            availableIndexes.splice(randomNumber, 1)
+        })
+        })
+    }
+
+
+
+// menu
+
+const navList = document.querySelector('.nav-list')
+
+navBurger.addEventListener('click', () => {
+    navBurger.classList.toggle('active')
+    navigation.classList.toggle('active')
+    logo.classList.toggle('active')
+    overlay.classList.toggle('active')
+    body.classList.toggle('disable-scroll')
+    html.classList.toggle('disable-scroll')
+    navList.classList.toggle('active')
+
+})
+
+navItems.forEach((element) => {
+    element.addEventListener('click', () => {
+        navBurger.classList.remove('active')
+        navigation.classList.remove('active')
+        logo.classList.remove('active')
+        overlay.classList.remove('active')
+        body.classList.remove('disable-scroll')
+        html.classList.remove('disable-scroll')
+        navList.classList.remove('active')
+    })
+})
+
+window.addEventListener('click', (event) => {
+    const target = event.target;
+    if (!target.closest('.menu')) {
+        navigation.classList.remove('active')
+        logo.classList.remove('active')
+        navBurger.classList.remove('active')
+        overlay.classList.remove('active')
+        body.classList.remove('disable-scroll')
+        html.classList.remove('disable-scroll')
+        navList.classList.remove('active')
+    }
+})
+
+// pets cards interaction
+
+petCards.forEach((item) => {
+    item.onmouseover = () => {
+        petCards.forEach((element) => {
+            if (element.classList.contains('active')) {
+                element.classList.remove('active')
+            }
+        })
+        item.classList.add('active')
+        item.querySelector('.our-friends__card-button').classList.add('active')
+
+    }
+    item.onmouseout = () => {
+        item.classList.remove('active')
+        item.querySelector('.our-friends__card-button').classList.remove('active')
+    }
+})
+
+// nav items interaction
+
+navItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        navItems.forEach((element) => {
+            if (element.classList.contains('active')) {
+                element.classList.remove('active')
+            }
+        })
+        item.classList.add('active')
+    })
+})
---
+++ pages/pets/script.js
@@ -0,0 +1,528 @@
+const petCards = document.querySelectorAll('.our-friends__card')
+const petList = document.querySelector('.our-friends__list')
+const navItems = document.querySelectorAll('.nav-list__item')
+const header = document.querySelector('.header')
+const sticky =  header.offsetTop;
+const navBurger = document.querySelector('.nav-burger')
+const navigation = document.querySelector('.nav')
+const logo = document.querySelector('.logo-wrapper')
+const overlay = document.querySelector('.overlay')
+
+const requestURL = '../pets/pets.json'
+
+// pagination
+
+const newURL = '../pets/pets-40.json'
+const paginationItems = document.querySelectorAll('.our-friends__pagination-item')
+const paginationLinks = document.querySelectorAll('.our-friends__pagination-link')
+const prevTwo = document.querySelector('.pagination-prev-2')
+const prevOne = document.querySelector('.pagination-prev-1')
+const nextOne = document.querySelector('.pagination-next-1')
+const nextTwo = document.querySelector('.pagination-next-2')
+const thisPage = document.querySelector('.pagination-now')
+
+let eightPets = []
+let sixPets = []
+let threePets = []
+
+eightPets.length = 8;
+sixPets.length = 6;
+threePets.length = 3;
+
+
+sendRequest('GET', requestURL)
+    .then((data) => {
+
+
+// наполнить каждый массив переменной, затем while array.includes(k) - randomise, мб сделать зависимость от количества активных карточек
+
+        // NUMBERS
+        let petLength = 0;
+        petCards.forEach((element) => {
+            if (getComputedStyle(element).getPropertyValue('display') !== 'none') {
+                petLength++;
+            }
+        })
+        console.log(petLength)
+        let all = 48 / petLength
+        let current = 1;
+
+        // basic rules
+        if (current === 1) {
+            prevOne.classList.add('disabled')
+            prevTwo.classList.add('disabled')
+        }
+
+        // next One
+        nextOne.addEventListener('click', (e) => {
+            prevOne.classList.remove('disabled')
+            prevTwo.classList.remove('disabled')
+            if (current !== all) {
+                current++;
+                console.log(current)
+                thisPage.textContent = `${current}`;
+            }
+            if (current === all) {
+                nextOne.classList.add('disabled')
+                nextTwo.classList.add('disabled')
+                e.preventDefault()
+            }
+        })
+        // next Two
+        nextTwo.addEventListener('click', (e) => {
+            nextTwo.classList.add('disabled')
+            nextOne.classList.add('disabled')
+            prevTwo.classList.remove('disabled')
+            prevOne.classList.remove('disabled')
+            e.preventDefault()
+            current = all;
+            thisPage.textContent = `${current}`;
+        })
+        // prev One
+        prevOne.addEventListener('click', (e) => {
+            nextOne.classList.remove('disabled')
+            nextTwo.classList.remove('disabled')
+            if (current !== 1) {
+                current--;
+                console.log(current)
+                thisPage.textContent = `${current}`;
+                prevOne.classList.remove('disabled')
+            }
+            if (current === 1) {
+                prevOne.classList.add('disabled')
+                prevTwo.classList.add('disabled')
+                e.preventDefault()
+            }
+        })
+        // prev Two
+        prevTwo.addEventListener('click', (e) => {
+            prevTwo.classList.add('disabled')
+            prevOne.classList.add('disabled')
+            nextTwo.classList.remove('disabled')
+            nextOne.classList.remove('disabled')
+            e.preventDefault()
+            current = 1;
+            thisPage.textContent = `${current}`;
+        })
+
+        //1001
+
+        let arrPets = [
+            ['Katrine' ],
+            ['Jennifer'],
+            ['Woody'],
+            ['Sophia'],
+            ['Timmy'],
+            ['Charly'],
+            ['Scarlett'],
+            ['Freddie'],
+        ]
+        let arrPets1 = [
+            ['Katrine' ],
+            ['Jennifer'],
+            ['Woody'],
+            ['Sophia'],
+            ['Timmy'],
+            ['Charly'],
+            ['Scarlett'],
+            ['Freddie'],
+        ]
+        let arrPets2 = [
+            ['Katrine' ],
+            ['Jennifer'],
+            ['Woody'],
+            ['Sophia'],
+            ['Timmy'],
+            ['Charly'],
+            ['Scarlett'],
+            ['Freddie'],
+        ]
+        let arrPets3 = [
+            ['Katrine' ],
+            ['Jennifer'],
+            ['Woody'],
+            ['Sophia'],
+            ['Timmy'],
+            ['Charly'],
+            ['Scarlett'],
+            ['Freddie'],
+        ]
+        let arrPets4 = [
+            ['Katrine' ],
+            ['Jennifer'],
+            ['Woody'],
+            ['Sophia'],
+            ['Timmy'],
+            ['Charly'],
+            ['Scarlett'],
+            ['Freddie'],
+        ]
+        let arrPets5 = [
+            ['Katrine' ],
+            ['Jennifer'],
+            ['Woody'],
+            ['Sophia'],
+            ['Timmy'],
+            ['Charly'],
+            ['Scarlett'],
+            ['Freddie'],
+        ]
+
+        function shuffle(arr) {
+            arr.sort(() => Math.random() - 0.5);
+            return arr;
+        }
+
+        let a = shuffle(arrPets);
+        let b = shuffle(arrPets1);
+        let c = shuffle(arrPets2);
+        let d = shuffle(arrPets3);
+        let e = shuffle(arrPets4);
+        let f = shuffle(arrPets5);
+        let x = [];
+        let arrNew = x.concat(a, b, c, d, e, f);
+
+        console.log(arrNew)
+
+        let n;
+
+        const screenWidth = window.screen.width;
+        if (screenWidth >= 1280) { n = 8; }
+        else if (screenWidth >= 768) { n = 6; }
+        else if (screenWidth < 768) { n = 3; }
+
+        let currentPage = 1;
+        let start = (currentPage - 1)*n;
+        let end = currentPage*n;
+
+        function createCard() {
+            let arr = arrNew.slice(start, end);
+            console.log('arr', arr)
+            for (let i = 0; i < n; i++) {
+
+                petCards[i].querySelector('.our-friends__card-title').innerHTML = arr[i][0]
+                let title = petCards[i].querySelector('.our-friends__card-title').textContent
+                data.forEach((item) => {
+                    if (item.name === arr[i][0]) {
+                        petCards[i].querySelector('.our-friends__card-img').src = item.img
+                    }
+                })
+
+
+        }
+
+        /*let petsIndexes = []
+        let randomPets = []
+        randomPets.length = 48;
+        let number = 0;
+        let start = 0;*/
+
+        /*let k = randomise(0, 7)
+        petsIndexes.push(k)*/
+
+        // записываем индексы элементов, которые уже есть
+       /* let array = [];
+
+        for (let s = 0; s < data.length; s++) {
+            petsIndexes.push(s)
+        }
+
+        console.log(petsIndexes)
+
+        petCards.forEach((item) => {
+            for (let t = 0; t < petLength; t++) {
+                let title = item.querySelector('.our-friends__card-title').textContent
+                if (data[t].name === title) {
+                    petsIndexes.splice(t, 1, '')
+                }
+            }
+        })
+        for (let p = 0; p < petsIndexes.length; p++) {
+            if (petsIndexes[p] === '') {
+                petsIndexes.splice(p, 1)
+                p -= 1;
+            }
+        }
+
+        array.unshift(current)*/
+
+
+
+       /* eightPets.push(k)
+        sixPets.push(k)
+        threePets.push(k)
+        randomPets.push(k)*/
+
+
+        /*for (let m = 0; m < petLength; m++) {
+            let k = randomise(0, 7)
+            /!*while (eightPets.includes(k) || sixPets.includes(k) || threePets.includes(k)) {
+                k = randomise(k)
+            }*!/
+            eightPets.push(k)
+            sixPets.push(k)
+            threePets.push(k)
+        }*/
+
+
+        /*for (let k = 0; k < 8; k++) {
+            randomPets.fill(number, start, start + 6)
+            start += 6
+            number++
+        }*/
+
+
+
+
+
+
+        /*randomPets.sort((a, b) => {
+            randomPets = Math.random() - 0.5
+        })*/
+
+        /*randomPets.sort(() => Math.random() - 0.5)*/
+
+
+
+        // проверка на повторение каждой 7 карточки
+
+
+
+    }
+        nextOne.addEventListener('click', () => {
+            currentPage += 1;
+            start = (currentPage - 1)*n;
+            end = currentPage*n;
+            createCard()
+        })
+        prevOne.addEventListener('click', () => {
+            currentPage -= 1;
+            start = (currentPage - 1)*n;
+            end = currentPage*n;
+            createCard();
+        })
+        prevTwo.addEventListener('click', () => {
+            currentPage = 1;
+            start = (currentPage - 1)*n;
+            end = currentPage*n;
+            createCard();
+        })
+        nextTwo.addEventListener('click', () => {
+            currentPage = all;
+            start = (currentPage - 1)*n;
+            end = currentPage*n;
+            createCard();
+        })
+    })
+
+function randomise(min, max) {
+    let result = Math.floor(Math.random() * (max - min) + min)
+    return result
+}
+
+paginationItems.forEach((item) => {
+    if (item.classList.contains('disabled')) {
+        item.classList.add('disabled')
+    }
+})
+
+
+
+
+// popup
+
+const popup = document.querySelector('.popup')
+const name = popup.querySelector('.popup-title')
+const breed = popup.querySelector('.popup-breed')
+const desc = popup.querySelector('.popup-desc')
+const age = popup.querySelector('.age')
+const inoculations = popup.querySelector('.inoculations')
+const diseases = popup.querySelector('.diseases')
+const parasites = popup.querySelector('.parasites')
+const popupImg = popup.querySelector('.popup-img')
+const popupBtn = document.querySelector('.popup-button')
+const body = document.querySelector('body');
+const html = document.querySelector('html');
+const overlayPopup = document.querySelector('.overlay')
+
+
+sendRequest('GET', requestURL)
+    .then ((data) => {
+        petCards.forEach((item) => {
+            item.addEventListener('click', () => {
+                console.log('click');
+                for (let d = 0; d < data.length; d++) {
+                    let title = item.querySelector('.our-friends__card-title').textContent
+                    if (data[d].name === title) {
+                        name.innerHTML = data[d].name
+                        breed.innerHTML = `${data[d].type}: ${data[d].breed}`
+                        desc.innerHTML = data[d].description
+                        age.innerHTML = `<span style="font-weight: 700">Age:</span> ${data[d].age}`
+                        inoculations.innerHTML = `<span style="font-weight: 700">Inoculations:</span> ${data[d].inoculations}`
+                        diseases.innerHTML = `<span style="font-weight: 700">Diseases:</span> ${data[d].diseases}`
+                        parasites.innerHTML = `<span style="font-weight: 700">Parasites:</span> ${data[d].parasites}`
+                        popupImg.src= data[d].img
+                    }
+                }
+                popup.classList.add('active')
+                body.classList.add('disable-popup-scroll')
+                html.classList.add('disable-popup-scroll')
+                overlayPopup.classList.add('active-popup')
+            })
+        })
+
+    })
+
+
+
+popupBtn.addEventListener('click', () => {
+    popup.classList.remove('active')
+    body.classList.remove('disable-popup-scroll')
+    html.classList.remove('disable-popup-scroll')
+    overlayPopup.classList.remove('active-popup')
+})
+
+document.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.classList.contains('overlay')) {
+        popup.classList.remove('active')
+        overlayPopup.classList.remove('active-popup')
+        body.classList.remove('disable-popup-scroll')
+        html.classList.remove('disable-popup-scroll')
+    }
+})
+
+document.addEventListener('mouseover', (event) => {
+    const target = event.target;
+    if (target.classList.contains('overlay')) {
+        popupBtn.classList.add('active')
+    }
+})
+document.addEventListener('mouseout', (event) => {
+    const target = event.target;
+    if (target.classList.contains('overlay')) {
+        popupBtn.classList.remove('active')
+    }
+})
+
+// json pets
+
+sendRequest('GET', requestURL)
+    .then ((data) => {
+        console.log('data', data[0])
+        for (let i = 0; i < petCards.length; i++) {
+            petCards[i].querySelector('.our-friends__card-title').innerHTML = data[i].name
+            petCards[i].querySelector('.our-friends__card-img').src = data[i].img
+
+        }
+        console.log(data)
+    })
+    .catch(err => console.log(err))
+
+
+
+function sendRequest(method, url) {
+    return new Promise ((resolve, reject) => {
+        const xhr = new XMLHttpRequest()
+        xhr.open(method, url)
+        xhr.responseType = 'json'
+
+        xhr.onload = () => {
+            if (xhr.status >= 400) {
+                reject(xhr.response)
+            } else {
+                resolve(xhr.response)
+            }
+        }
+        xhr.onerror = () => {
+            reject(xhr.response)
+        }
+        xhr.send()
+    })
+
+}
+
+// menu
+const navList = document.querySelector('.nav-list')
+
+navBurger.addEventListener('click', () => {
+    navBurger.classList.toggle('active')
+    navigation.classList.toggle('active')
+    logo.classList.toggle('active')
+    overlay.classList.toggle('active')
+    body.classList.toggle('disable-scroll')
+    html.classList.toggle('disable-scroll')
+    navList.classList.toggle('active')
+})
+
+navItems.forEach((element) => {
+    element.addEventListener('click', () => {
+        navBurger.classList.remove('active')
+        navigation.classList.remove('active')
+        logo.classList.remove('active')
+        overlay.classList.remove('active')
+        body.classList.remove('disable-scroll')
+        html.classList.remove('disable-scroll')
+        navList.classList.remove('active')
+    })
+})
+
+window.addEventListener('click', (event) => {
+    const target = event.target;
+    console.log(target)
+    if (!target.closest('.menu')) {
+        navigation.classList.remove('active')
+        logo.classList.remove('active')
+        navBurger.classList.remove('active')
+        overlay.classList.remove('active')
+        body.classList.remove('disable-scroll')
+        html.classList.remove('disable-scroll')
+        navList.classList.remove('active')
+    }
+})
+
+// sticky header
+
+window.onscroll = function() {stickyHeader()};
+
+function stickyHeader() {
+    if (window.pageYOffset >= sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+
+// pet cards interaction
+
+petCards.forEach((item) => {
+    item.onmouseover = () => {
+        console.log('mouse on')
+        petCards.forEach((element) => {
+            if (element.classList.contains('active')) {
+                element.classList.remove('active')
+            }
+        })
+        item.classList.add('active')
+        item.querySelector('.our-friends__card-button').classList.add('active')
+
+    }
+    item.onmouseout = () => {
+        item.classList.remove('active')
+        item.querySelector('.our-friends__card-button').classList.remove('active')
+    }
+})
+
+// nav items interaction
+
+navItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        navItems.forEach((element) => {
+            if (element.classList.contains('active')) {
+                element.classList.remove('active')
+            }
+        })
+        item.classList.add('active')
+    })
+})
